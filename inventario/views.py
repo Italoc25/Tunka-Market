95adef71b5194bcb5b10881eef05901285c0e507
Italo <italo.castillo2500@gmail.com>
2026-06-06 19:55:56 -0400

    Cambio de ip3

--- inventario/views.py
+++ inventario/views.py
@@ -63,7 +63,7 @@ def verificador_precios(request):
     # IPs autorizadas (Agregamos la nueva detectada por Railway)
     IPV4_TUNKA_TIENDA = "200.111.224.125"
     IPV4_TUNKA_RAILWAY = "186.10.141.46" 
-    PREFIJO_IPV6_TUNKA = "198.41.233.12"
+    PREFIJO_IPV6_TUNKA = "104.23.237.39"
 
     # Obtener la IP real
     x_forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
